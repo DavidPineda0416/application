b3ce70cc980b03125b3a77d78e363d3019b4ed3f
David Pineda <deibittop@gmail.com>
2024-06-29 20:41:34 +0900

    とりあえず完成 チェック入れるとバグ発生

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,14 +1,49 @@
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
-
+//フックス　//プロップス
 function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, name: "Todo1" , complated: false},
+  ]);
+  const todoNmaeRef = useRef();
+
+
+  //タスクを追加する
+  const handleAddTodo = () =>{
+    const name = todoNmaeRef.current.value;
+    if(name === "")return; //空白の場合の処理のストップ
+    setTodos((prevTodos) => {
+      return[...prevTodos, {id: 1, name: name , complated: false}];
+    });
+    todoNmaeRef.current.value = null;
+  };
+
+
+  //一致するIDを見つけてくる
+  const toggleTodo = (id) =>{
+    const newTodos = {...todos};
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complated = !todo.complated;
+    setTodos(newTodos);
+  };
+
+
+//残りのタスクを表示
+  const handleClear = () =>{
+    const newTodos = todos.filter((todo) => !todo.complated );
+    setTodos(newTodos);
+  };
+
+
+  //表示画面
   return( 
     <>
-      <TodoList />
-      <input type="text"/>
-      <button>タスク追加</button>
-      <button>完了したタスク</button>
-      <div>残りのタスク</div>
+      <TodoList  todos = {todos} toggleTodo = {toggleTodo}/>
+      <input type="text" ref={todoNmaeRef}/>
+      <button onClick={handleAddTodo}>タスク追加</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.complated).length}</div>
     </>
     ); 
   }
